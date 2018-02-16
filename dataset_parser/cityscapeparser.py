e053753da2fefdb97e388ebada867c09f6230a11
tkwoo <wtk1101@gmail.com>
2018-02-16 22:10:49 +0900

    [update] name loop

--- dataset_parser/cityscapeparser.py
+++ dataset_parser/cityscapeparser.py
@@ -19,33 +19,30 @@ def select_labels(gt):
     return gt_new
 
 for area in area_img_list:
-    print (area)
+    # print (area)
     image_list = sorted(glob(os.path.join(area,'*')))
     # print (image_list)
     for image_name in image_list:
-        print (image_name)
         gt_name = os.path.basename(image_name).replace('leftImg8bit', 'gtFine_labelIds')
         gt_name = os.path.join(area_gt_path, os.path.basename(area), gt_name)
-        print (gt_name)
-        break
-    break
+        print (os.path.basename(image_name))
 
-img = cv2.imread(image_name, 1)
-gt = cv2.imread(gt_name, 1)
+        img = cv2.imread(image_name, 1)
+        gt = cv2.imread(gt_name, 1)
 
-img = cv2.resize(img, (img.shape[1]/2, img.shape[0]/2))
-gt = cv2.resize(gt, (gt.shape[1]/2, gt.shape[0]/2), interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(img, (img.shape[1]/2, img.shape[0]/2))
+        gt = cv2.resize(gt, (gt.shape[1]/2, gt.shape[0]/2), interpolation=cv2.INTER_NEAREST)
 
-gt = select_labels(gt)
+        gt = select_labels(gt)
 
-show = img
-gt_show = gt.astype(np.float32)*255/33
-gt_show = gt_show.astype(np.uint8)
-gt_show = cv2.applyColorMap(gt_show, cv2.COLORMAP_JET)
-show = cv2.addWeighted(img, 0.5, gt_show, 0.6, 0)
+        show = img
+        gt_show = gt.astype(np.float32)*255/33
+        gt_show = gt_show.astype(np.uint8)
+        gt_show = cv2.applyColorMap(gt_show, cv2.COLORMAP_JET)
+        show = cv2.addWeighted(img, 0.5, gt_show, 0.6, 0)
 
-cv2.imshow('gt', gt)
-cv2.imshow('show', show)
-cv2.waitKey()
-
-exit()
+        # cv2.imshow('gt', gt)
+        cv2.imshow('show', show)
+        key = cv2.waitKey()
+        if key == 27:
+            exit()
