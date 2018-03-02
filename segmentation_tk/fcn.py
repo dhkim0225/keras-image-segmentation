196a1ea8dc8c4a9fe543a65e9d323ca98c8beba6
tkwoo <wtk1101@gmail.com>
2018-03-02 11:52:14 +0900

    [add] fcn v0.1

---
+++ segmentation_tk/fcn.py
@@ -0,0 +1,166 @@
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, Activation
+from keras.layers import BatchNormalization, Add, Flatten, Dense, Dropout
+from keras.optimizers import Adam
+from keras import backend as K
+
+def dice_coef(y_true, y_pred):
+    smooth = 1.
+    y_true /= 255.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def pixelwise_l2_loss(y_true, y_pred):
+    y_true /= 255.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    return K.mean(K.square(y_true_f - y_pred_f))
+
+def pixelwise_binary_ce(y_true, y_pred):
+    y_true /= 255.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    return K.mean(K.binary_crossentropy(y_true_f, y_pred_f))
+
+def get_fcn8(flag):
+    img_rows = flag.image_height
+    img_cols = flag.image_width
+    lr = flag.initial_learning_rate
+    nClasses = 3
+
+    inputs = Input((img_rows, img_cols, 3))
+    x = Conv2D(32, (3, 3), activation=None, padding='same', name='block1_conv1')(inputs)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(32, (3, 3), activation=None, padding='same', name='block1_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    p1 = x
+    # Block 2
+    x = Conv2D(64, (3, 3), activation=None, padding='same', name='block2_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(64, (3, 3), activation=None, padding='same', name='block2_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    p2 = x
+
+    # Block 3
+    x = Conv2D(128, (3, 3), activation=None, padding='same', name='block3_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), activation=None, padding='same', name='block3_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(128, (3, 3), activation=None, padding='same', name='block3_conv3')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    p3 = x
+
+    # Block 4
+    x = Conv2D(256, (3, 3), activation=None, padding='same', name='block4_conv1')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), activation=None, padding='same', name='block4_conv2')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(256, (3, 3), activation=None, padding='same', name='block4_conv3')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    p4 = x
+
+    # Block 5
+    # x = Conv2D(512, (3, 3), activation=None, padding='same', name='block5_conv1')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), activation=None, padding='same', name='block5_conv2')(x)
+    # x = BatchNormalization()(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(512, (3, 3), activation=None, padding='same', name='block5_conv3')(x)
+    # x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    # f5 = x
+
+    # x = Flatten(name='flatten')(x)
+    # x = Dense(4096, activation=None, name='fc1')(x)
+    # x = Dense(4096, activation=None, name='fc2')(x)
+    # x = Dense(1000 , activation='softmax', name='predictions')(x)
+
+    vgg  = Model(inputs , x)
+    # vgg.summary()
+    # exit()
+    # vgg.load_weights(VGG_Weights_path)
+
+    ### pool4 (32,64,256) --> up1 (64,128,256)
+    o = p4
+    o = Conv2D(256, (5, 5), activation=None, padding='same')(o)
+    o = BatchNormalization()(o)
+    o = Activation('relu')(o)
+    o = Conv2D(256, (3, 3), activation=None, padding='same')(o)
+    o = BatchNormalization()(o)
+    o = Activation('relu')(o)
+    o = Conv2DTranspose(256, kernel_size=(4, 4), strides=(2, 2), padding='same')(o)
+
+    ### pool3 (64,32,128) --> (64,128,256)
+    o2 = p3
+    o2 = Conv2D(256, (3, 3), activation=None, padding='same')(o2)
+    o2 = BatchNormalization()(o2)
+    o2 = Activation('relu')(o2)
+    
+    ### concat [(64,128,256), (64,128,256)]
+    o = concatenate([o, o2], axis=3)
+    o = Conv2D(256, (3, 3), padding='same')(o)
+    o = BatchNormalization()(o)
+    o = Activation('relu')(o)
+
+    ### up2 (64,128,128) --> (128,256,128)
+    o = Conv2DTranspose(128, kernel_size=(4, 4), strides=(2, 2), padding='same')(o)
+
+    ### pool2 (128,256,64) --> (128,256,128)
+    o2 = p2
+    o2 = Conv2D(128, (3, 3), activation=None, padding='same')(o2)
+    o2 = BatchNormalization()(o2)
+    o2 = Activation('relu')(o2)
+
+    ### concat [up2, o2]
+    o = concatenate([o, o2], axis=3)
+    
+    o = Conv2DTranspose(64, kernel_size=(2, 2), strides=(2, 2), padding='same')(o)
+    o = Conv2D(64, (3, 3), padding='same')(o)
+    o = BatchNormalization()(o)
+    o = Activation('relu')(o)
+
+    o2 = p1
+    o2 = Conv2D(32, (3, 3), activation=None, padding='same')(o2)
+    o2 = BatchNormalization()(o2)
+    o2 = Activation('relu')(o2)
+
+    o = concatenate([o, o2], axis=3)
+    o = Conv2D(32, (3, 3), padding='same')(o)
+    o = BatchNormalization()(o)
+    o = Activation('relu')(o)
+
+    o = Conv2DTranspose(32, kernel_size=(2, 2), strides=(2, 2), padding='same')(o)
+    o = Conv2D(nClasses, (3, 3), padding='same')(o)
+    o = Activation('sigmoid')(o)
+    
+    model = Model(inputs=[inputs], outputs=[o])
+    
+    model.compile(optimizer=Adam(lr=lr, decay=1e-6), loss=pixelwise_binary_ce, metrics=[dice_coef])
+
+    return model
+
+if __name__ == "__main__":
+    class structure:
+        image_width = 512
+        image_height = 256
+        initial_learning_rate = 0.007
+    flag = structure()
+    # model = get_unet(flag)
+    model = get_fcn8(flag)
+    model.summary()
--- segmentation_tk/train.py
+++ segmentation_tk/train.py
@@ -14,6 +14,7 @@ import random
 import math
 
 from model import get_unet
+from fcn import get_fcn8
 import callbacks
 
 class TrainModel:
@@ -111,7 +112,9 @@ class TrainModel:
         config.gpu_options.allow_growth = True
         set_session(tf.Session(config=config))
 
-        model = get_unet(self.flag)
+        # model = get_unet(self.flag)
+        model = get_fcn8(self.flag)
+        
         if self.flag.pretrained_weight_path != None:
             model.load_weights(self.flag.pretrained_weight_path)
         
